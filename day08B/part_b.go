d8708df4aa7cdef1f1fc052a9572c87434a89de8
Jordi Izquierdo <jordi.izquierdo@unblur.co>
2023-12-08 16:47:17 +0100

    Unexpected LCM update

--- day08B/part_b.go
+++ day08B/part_b.go
@@ -7,85 +7,98 @@ import (
 	"strings"
 )
 
-var (
-	idRegex          = regexp.MustCompile(`[A-Z|1-9]{3}`)
-	startNodeIdRegex = regexp.MustCompile(`..A`)
-	endNodeIdRegex   = regexp.MustCompile(`..Z`)
-)
-
 type Node struct {
 	Id    string
-	Left  string
-	Right string
-}
-
-type Ghost struct {
-	NodeId string
+	Left  *Node
+	Right *Node
 }
 
 func main() {
-	input, err := os.ReadFile("input_b.txt")
+	input := ReadInput("input_b.txt")
+	instructions, starterNodes := ParseInput(input)
+
+	stepsPerGhost := make([]int, len(starterNodes))
+	for i := 0; i < len(starterNodes); i++ {
+		steps := 0
+		for j := 0; strings.LastIndex(starterNodes[i].Id, "Z") != 2; j++ {
+			// make sure we don't go out of bounds
+			j %= len(instructions)
+			// move the ghost
+			steps++
+			// take the next instruction
+			if instructions[j] == "R" {
+				starterNodes[i] = starterNodes[i].Right
+			} else {
+				starterNodes[i] = starterNodes[i].Left
+			}
+		}
+		stepsPerGhost[i] = steps
+	}
+
+	fmt.Println(HandleSteps(stepsPerGhost))
+}
+
+func ReadInput(path string) string {
+	input, err := os.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
 
-	inputString := string(input)
-	inputLines := strings.Split(inputString, "\n\r\n")
+	return string(input)
+}
 
-	// Actual data
+func ParseInput(input string) ([]string, []*Node) {
+	inputLines := strings.Split(input, "\n\r\n")
+
+	// Intructions are a string of R's and L's
 	instructions := strings.Split(strings.TrimSpace(inputLines[0]), "")
-	nodes := strings.Split(inputLines[1], "\n")
+	// Nodes are represented as a string of 3 characters with 2 more for each direction
+	nodeLines := strings.Split(inputLines[1], "\n")
 
-	// Creating the map of nodes and a list of ghosts
-	var ghosts []*Ghost
-	mapNodes := make(map[string]Node)
+	nodes := make(map[string]*Node, len(nodeLines))
+	starterNodes := make([]*Node, 0)
+	re := regexp.MustCompile(`[A-Z|1-9]{3}`)
 
-	// Populating the map of nodes and ghosts to start with
-	for _, node := range nodes {
-		matches := idRegex.FindAllString(node, 3)
-		node := Node{
-			Id:    matches[0],
-			Left:  matches[1],
-			Right: matches[2],
+	// Populate the nodes with their ids
+	for _, n := range nodeLines {
+		nodeId := re.FindString(n)
+		nodes[nodeId] = &Node{
+			Id: nodeId,
 		}
 
-		if startNodeIdRegex.MatchString(node.Id) {
-			ghosts = append(ghosts, &Ghost{
-				NodeId: node.Id,
-			})
-		}
-
-		mapNodes[node.Id] = node
-	}
-
-	step := 1
-outerLoop:
-	for {
-		for _, direction := range instructions {
-			for _, ghost := range ghosts {
-				fmt.Println(step, direction, ghost)
-
-				if direction == "R" {
-					ghost.NodeId = mapNodes[ghost.NodeId].Right
-				} else {
-					ghost.NodeId = mapNodes[ghost.NodeId].Left
-				}
-			}
-
-			ghostEnd := 0
-			for _, ghostPosition := range ghosts {
-				if endNodeIdRegex.MatchString(ghostPosition.NodeId) {
-					ghostEnd++
-				}
-			}
-
-			if ghostEnd == len(ghosts) {
-				break outerLoop
-			} else {
-				step++
-			}
+		if strings.LastIndex(nodeId, "A") == 2 {
+			starterNodes = append(starterNodes, nodes[nodeId])
 		}
 	}
 
-	fmt.Println("Day 8, Part B: ", step)
+	for _, n := range nodeLines {
+		matches := re.FindAllString(n, 3)
+		nodes[matches[0]].Left = nodes[matches[1]]
+		nodes[matches[0]].Right = nodes[matches[2]]
+	}
+
+	return instructions, starterNodes
+}
+
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+func LCM(a, b int) int {
+	return a * b / GCD(a, b)
+}
+
+func HandleSteps(numbers []int) int {
+	result := numbers[0]
+
+	for i := 1; i < len(numbers); i++ {
+		result = LCM(result, numbers[i])
+	}
+
+	return result
 }
