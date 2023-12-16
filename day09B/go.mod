e1f4a9bdf07d683b54fc4ca89e50af5331354b56
Jordi Izquierdo <jordi.izquierdo@unblur.co>
2023-12-16 14:09:02 +0100

    Day 9, second part done

---
+++ day09B/go.mod
@@ -0,0 +1,3 @@
+module day09B
+
+go 1.21.5
---
+++ day09B/part_b.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := ReadInput("input_b.txt")
+	histories := ParseInput(input)
+
+	var nextValues []int
+	for _, history := range histories {
+		// Sequence index
+		i := 0
+		// Sequence of differences
+		sequences := [][]int{history}
+		// The next sequence for each prediction
+		nextSequence := make([]int, len(history)-1)
+
+		// Prediction loops
+		for {
+			for j := 0; j < len(sequences[i])-1; j++ {
+				nextSequence[j] = sequences[i][j+1] - sequences[i][j]
+			}
+
+			sequences = append(sequences, nextSequence)
+
+			// Check if we're done
+			sum := 0
+			for _, stepValue := range nextSequence {
+				if stepValue < 0 {
+					sum -= stepValue
+				} else {
+					sum += stepValue
+				}
+			}
+
+			i++
+			if sum == 0 {
+				// Adding a 0 to the next sequence before extrapolating
+				sequences[i] = append([]int{0}, sequences[i]...)
+				break
+			} else {
+				// Setting an empty new array with a known lenght
+				nextSequence = make([]int, len(sequences[i])-1)
+			}
+		}
+
+		// Extrapolation loops
+		for {
+			currentSequence := sequences[i][0]
+			prevSequence := sequences[i-1][0]
+			sequences[i-1] = append([]int{prevSequence - currentSequence}, sequences[i-1]...)
+
+			if i == 1 {
+				break
+			} else {
+				i--
+			}
+		}
+
+		nextValue := sequences[0][0]
+		nextValues = append(nextValues, nextValue)
+	}
+
+	result := 0
+	for _, curr := range nextValues {
+		result += curr
+	}
+
+	fmt.Println("Result 🎊", result)
+}
+
+func ReadInput(path string) string {
+	input, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(input)
+}
+
+func ParseInput(input string) [][]int {
+	inputLines := strings.Split(input, "\n")
+	histories := make([][]int, len(inputLines))
+
+	for i := 0; i < len(inputLines); i++ {
+		// Getting values from the line as strings
+		trimmedLine := strings.TrimSpace(inputLines[i])
+		splittedLine := strings.Split(trimmedLine, " ")
+
+		// Converting the strings to ints
+		for _, item := range splittedLine {
+			num, err := strconv.Atoi(item)
+			if err != nil {
+				panic(err)
+			} else {
+				histories[i] = append(histories[i], num)
+			}
+		}
+	}
+
+	return histories
+}
